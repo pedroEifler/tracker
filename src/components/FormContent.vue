8e1590e852d30a03aa389658638bd5033cb875f8
Pedro Henrique Eifler <62551886+pedroEifler@users.noreply.github.com>
2022-09-23 21:02:59 -0300

    Layout Base

---
+++ src/components/FormContent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form">
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+            <section>
+              <strong>00:00:00</strong>
+            </section>
+            <button class="button">
+              <span class="icon">
+                <i class="fas fa-play"></i>
+              </span>
+              <span>play</span>
+            </button>
+            <button class="button">
+              <span class="icon">
+                <i class="fas fa-stop"></i>
+              </span>
+              <span>stop</span>
+            </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormContent",
+});
+</script>
+  
+<style scoped>
+</style>
